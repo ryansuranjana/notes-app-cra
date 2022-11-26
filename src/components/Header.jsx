161f0707ebb9a91f0eee6dbcf8f22c8e609e6107
ryansuranjana <ryansuranjana23@gmail.com>
2022-11-26 16:50:35 +0800

    install rodal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { AiOutlinePlus } from "react-icons/ai"
 
 const Header = () => {
   return (
     <>
       <div className='w-full p-5 text-center bg-[#16181D]'>
         <h1 className='text-3xl font-bold text-[#61DAFB]'>Notes App</h1>
-      </div>
-      <div className='w-full bg-[#20232A] text-center p-5'>
-          <input className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Search Notes" />
-          <button className="bg-gray-300 ml-2 hover:bg-gray-400 text-gray-800 font-bold py-3 px-4 rounded inline-flex items-center">
-            <AiOutlinePlus />
-          </button>
       </div>
     </>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NotesActive from './NotesActive'
 import NotesArchive from './NotesArchive'
+import { AiOutlinePlus } from "react-icons/ai"
+import Rodal from 'rodal';
+import 'rodal/lib/rodal.css';
 
 const Main = () => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const showModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
   return (
-    <div className='w-4/5 m-auto'>
+    <>
+      <div className='w-full bg-[#20232A] text-center p-5'>
+          <input className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Search Notes" />
+          <button className="bg-gray-300 ml-2 hover:bg-gray-400 text-gray-800 font-bold py-3 px-4 rounded inline-flex items-center" onClick={showModal}>
+            <AiOutlinePlus />
+          </button>
+      </div>
+      <Rodal visible={modalIsOpen} onClose={closeModal} animation="fade" width="500">
+          <div>Content</div>
+      </Rodal>
+      <div className='w-4/5 m-auto'>
         <NotesActive />
         <NotesArchive />
-    </div>
+      </div>
+    </>
   )
 }
 

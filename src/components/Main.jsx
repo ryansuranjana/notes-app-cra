3948183f6f155db37efb4c452fa0d6a7ee0929a4
ryansuranjana <ryansuranjana23@gmail.com>
2022-11-27 12:45:06 +0800

    fix responsive modal

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,7 +22,7 @@ const Main = () => {
             <AiOutlinePlus />
           </button>
       </div>
-      <Rodal visible={modalIsOpen} onClose={closeModal} animation="fade" width="370" height="270">
+      <Rodal visible={modalIsOpen} onClose={closeModal} animation="fade" width={370} height={270}>
           <div>
             <h3>{modalTitle}</h3>
           </div>
@@ -34,7 +34,7 @@ const Main = () => {
               <div>
                 <textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:border-[#61DAFB]" placeholder="Write your note here" defaultValue={""} />
               </div>
-              <button type="submit" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 my-2">Save</button>
+              <button type="submit" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 mr-2 my-2">Save</button>
             </div>
           </form>
       </Rodal>

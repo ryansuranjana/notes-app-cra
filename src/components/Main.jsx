6c37f8e7bd7b87de096ba61075708ad1a9f8cc99
ryansuranjana <ryansuranjana23@gmail.com>
2022-11-27 17:14:21 +0800

    make function handle submit create note

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NotesActive from './NotesActive'
 import NotesArchive from './NotesArchive'
 import { AiOutlinePlus } from "react-icons/ai"
-import Rodal from 'rodal';
-import 'rodal/lib/rodal.css';
+import Rodal from 'rodal'
+import 'rodal/lib/rodal.css'
+import Storagebase from '../utils/Storagebase'
+import { nanoid } from 'nanoid'
 
 const Main = () => {
   const [modalIsOpen, setIsOpen] = useState(false)
   const [modalTitle, setModalTitle] = useState('Add Note')
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
   const showModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
+
+  useEffect(() => {
+    const notes = Storagebase.get('NOTES')
+    if(notes) {
+      setNotes(notes)
+    } else {
+      localStorage.setItem('NOTES', JSON.stringify([]))
+    }
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const note = {
+      id: nanoid(),
+      title,
+      body,
+      created_at: new Date(),
+      archived: false
+    }
+    Storagebase.store('NOTES', note)
+    setNotes(Storagebase.get('NOTES'))
+    setIsOpen(false)
+  }
 
   return (
     <>
@@ -26,21 +54,21 @@ const Main = () => {
           <div>
             <h3>{modalTitle}</h3>
           </div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='mt-5'>
               <div className='mb-2'>
-                <input type="text" id="first_name" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-[#61DAFB] block w-full p-2.5" placeholder="Title" required maxLength={50}/>
+                <input type="text" id="title" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-[#61DAFB] block w-full p-2.5" placeholder="Title" required maxLength={50} value={title} onChange={(event) => setTitle(event.target.value)}/>
               </div>
               <div>
-                <textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:border-[#61DAFB]" placeholder="Write your note here" defaultValue={""} />
+                <textarea id="description" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:border-[#61DAFB]" placeholder="Write your note here" value={body} onChange={(event) => setBody(event.target.value)}/>
               </div>
               <button type="submit" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm md:px-5 px-3 md:py-2.5 py-2 mr-2 my-2">Save</button>
             </div>
           </form>
       </Rodal>
       <div className='w-4/5 m-auto'>
-        <NotesActive />
-        <NotesArchive />
+        <NotesActive notes={notes}/>
+        <NotesArchive notes={notes}/>
       </div>
     </>
   )
--- src/components/NotesArchive.jsx
+++ src/components/NotesArchive.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 
-const NotesArchive = () => {
-  return (
-    <div className='mb-16'>
-        <h3 className='font-bold md:text-2xl text-xl'>Archive</h3>
-            <div className='grid md:grid-cols-3 gap-3 md:mt-5 mt-3'>
-                <div className="max-w-sm h-max rounded overflow-hidden shadow-lg border border-[#61DAFB]">
-                    <div className="px-6 py-4">
-                        <div className="font-bold md:text-xl text-lg mb-2">The Coldest Sunset</div>
-                        <p className='md:text-sm text-xs font-light -mt-2 mb-3'>Kamis, 14 April 2022</p>
-                        <p className="text-gray-700 md:text-base text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                        </p>
-                    </div>
-                    <div className="px-6 md:pt-4 pt-1 md:pb-5 pb-4">
-                        <div>
-                            <button className="bg-transparent hover:bg-orange-400  text-orange-400 md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-orange-400  hover:border-transparent rounded mr-2">
-                                Edit
-                            </button>
-                            <button className="bg-transparent hover:bg-red-600 text-red-600 md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-red-600 hover:border-transparent rounded mr-2">
-                                Delete
-                            </button>
-                            <button className="bg-transparent hover:bg-[#61DAFB] text-[#61DAFB] md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-[#61DAFB] hover:border-transparent rounded">
-                                Unarchive
-                            </button>
-                        </div>
-                    </div>
+const NotesArchive = ({ notes }) => {
+    return (
+        <div className='mb-40'>
+            <h3 className='font-bold md:text-2xl text-xl'>Archive</h3>
+            {notes.length !== 0 ? (
+                <div className='grid md:grid-cols-3 gap-3 md:mt-5 mt-3'>
+                    {notes.filter((note) => note.archived === true).map((note) => 
+                        (
+                            <div className="max-w-sm h-max rounded overflow-hidden shadow-lg border border-[#61DAFB]">
+                                <div className="px-6 py-4">
+                                    <div className="font-bold md:text-xl text-lg mb-2">The Coldest Sunset</div>
+                                    <p className='md:text-sm text-xs font-light -mt-2 mb-3'>Kamis, 14 April 2022</p>
+                                    <p className="text-gray-700 md:text-base text-sm">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                                    </p>
+                                </div>
+                                <div className="px-6 md:pt-4 pt-1 md:pb-5 pb-4">
+                                    <div>
+                                        <button className="bg-transparent hover:bg-orange-400  text-orange-400 md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-orange-400  hover:border-transparent rounded mr-2">
+                                            Edit
+                                        </button>
+                                        <button className="bg-transparent hover:bg-red-600 text-red-600 md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-red-600 hover:border-transparent rounded mr-2">
+                                            Delete
+                                        </button>
+                                        <button className="bg-transparent hover:bg-[#61DAFB] text-[#61DAFB] md:font-semibold md:text-base text-sm hover:text-white py-2 md:px-4 px-3 border border-[#61DAFB] hover:border-transparent rounded">
+                                            Unarchive
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    )}
                 </div>
-            </div>
-    </div>
-  )
+            ) : (
+                <p className='mt-10 text-center'>Notes not found.</p>
+            )}
+        </div>
+    )
 }
 
 export default NotesArchive

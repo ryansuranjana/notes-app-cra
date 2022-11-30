c0669d9503fbdc0099c510a0e0862f80b4b7555e
ryansuranjana <ryansuranjana23@gmail.com>
2022-11-30 22:56:45 +0800

    fix modal

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -84,7 +84,7 @@ const Main = () => {
                   <span className="sr-only">Close modal</span>
                 </button>
                 <svg className="text-gray-400 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" /></svg>
-                <p className="mb-4 text-gray-500">Are you sure you want to delete this item?</p>
+                <p className="mb-4 text-gray-500">Are you sure you want to delete this note?</p>
                 <div className="flex justify-center items-center space-x-4">
                   <button data-modal-toggle="deleteModal" type="button" className="py-2 px-3 text-sm font-medium text-gray-300 bg-gray-700 rounded-lg border border-gray-200 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-primary-300 focus:z-10" onClick={onClose}>
                     No, cancel
